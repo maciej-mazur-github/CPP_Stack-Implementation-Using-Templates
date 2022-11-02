8b2efb82771df6f0ac9613f7cda15a43f1da1e1b
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-11-02 11:38:58 +0100

    Correct loadArrayOfObjects(T arr[], int arraySize), overload changeStackCapacity()

--- Stack Implementation Using Templates/main.cpp
+++ Stack Implementation Using Templates/main.cpp
@@ -11,13 +11,12 @@ using namespace std;
 
 int main()
 {
-	Stack<int, 8> ex1;
-	Stack<int, 5> ex2;
+	Stack<int, 8> stack1("stack1");
 
 	int a = 0;
 
 	cout << "*****************************";
-	cout << "\nInitiating ex1 Stack:" << endl;
+	cout << "\nInitiating Stack<int, 8> stack1:" << endl;
 	int intArray[10];
 
 	for (int i = 0; i < 10; i++)
@@ -25,70 +24,110 @@ int main()
 		intArray[i] = i * 2;
 	}
 
-	ex1.loadArrayOfObjects(intArray, 10);
-	cout << "\n*****************************";
+	stack1.loadArrayOfObjects(intArray, 10);
+
+	cout << "\n\n*****************************";
+	cout << "\nCurrent stack1 status:" << endl;
+	cout << stack1;
+
+	cout << endl << endl;
 
 
 	cout << "\n\n*****************************";
-	cout << "\nCurrent ex1 status:" << endl;
-	cout << ex1;
-	cout << "\n*****************************";
+	cout << "\nInitiating Stack<int, 6> stack2 using stack1:" << endl;
+	Stack<int, 6> stack2("stack2");
 
-
-	cout << "\n\n*****************************";
-	cout << "\nInitiating ex2 Stack using ex1:" << endl;
-	int elementNumber = ex1.getElementCounter();
-
-	for (int i = 0; i < elementNumber; i++)
+	while (!stack1.empty())
 	{
-		ex2.push(ex1.pop());
+		stack2.push(stack1.pop());
 		cout << ".";
 	}
-	cout << "\n*****************************";
+
 
 	cout << "\n\n*****************************";
-	cout << "\nCurrent ex1 status:" << endl;
-	cout << ex1;
-	cout << "\n*****************************";
+	cout << "\nCurrent stack1 status:" << endl;
+	cout << stack1;
+
 	cout << "\n\n*****************************";
-	cout << "\nCurrent ex2 status:" << endl;
-	cout << ex2;
+	cout << "\nCurrent stack2 status:" << endl;
+	cout << stack2;
 	cout << "\n*****************************";
 
-	Stack <string, 10> ex3;
+	cout << "\nInitiating Stack<string, 10> stack3:" << endl;
+	Stack <string, 10> stack3("stack3");
 
-	string a1 = "Loma", b1 = "Chenko", c1 = "Foxtrot";
+	string a1 = "Samba", b1 = "Jive", c1 = "Foxtrot";
 
-	ex3.push(a1);
-	ex3.push(b1);
-	ex3.push(c1);
+	stack3.push(a1);
+	stack3.push(b1);
+	stack3.push(c1);
+	cout << "\nCurrent stack3 status:" << endl;
+	cout << stack3;
+	cout << "\n*****************************";
+	cout << "\nNow popping all the elements of stack3:\n";
+
+	while (!stack3.empty())
+	{
+		cout << "\t" << stack3.pop() << endl;
+	}
 
 
 	cout << "\n*****************************";
-	cout << "\nCurrent ex3 status:" << endl;
-	cout << ex3;
+	cout << "\nCurrent stack3 status:" << endl;
+	cout << stack3;
 	cout << "\n*****************************\n";
 
-	ex3.pop();
-	ex3.pop();
-	cout << ex3.pop();
+	stack3.pop();
+	stack3.pop();
+	cout << stack3.pop();
 
 
-	Stack<Person, 3> ex4;
-
-	cout << "\n\n************************************\n";
-	cout << "Creating array of Person and pushing each person to stack ex4:\n";
+	cout << "\n*****************************\n";
+	cout << "\nInitiating Stack<Person, 3> stack4:" << endl;
+	Stack<Person, 3> stack4("stack4");
+	cout << "\nCreating array of Person objects and pushing each person to stack4:\n";
 
 	Person personArray [] = {Person("John"), Person("Peter"), Person("Adam"), Person("Kate")};
 
 	personArray[3] = "Mary";
 
-	ex4.loadArrayOfObjects(personArray, 4);
+	stack4.loadArrayOfObjects(personArray, 4);
 
 	cout << "\n*****************************\n";
 
-	cout << "\nCurrent ex4 status:" << endl;
-	cout << ex4;
-	cout << "\n*****************************";
+	cout << "\nCurrent stack4 status:" << endl;
+	cout << stack4;
+	cout << "\n*****************************\n";
+	cout << "Adding another 4-element array of Person objects to the pre-existing stack4:\n";
+	Person anotherPersonArray[] = {Person("Ron"), Person("Lilly"), Person("Rollo"), Person("Shaun"), Person("Cecille")};
+	stack4.loadArrayOfObjects(anotherPersonArray, 5);
+	cout << "\n*****************************\n";
+	cout << "Current stack4 status:" << endl;
+	cout << stack4;
+	cout << "\nAdding another 7-element array of Person objects to the pre-existing stack4:\n";
+	Person yetAnotherPersonArray[] = { Person("Jimmie"), Person("Nel"), Person("Rowan"), Person("Liam"), Person("Felix"), Person("Suzie"), Person("Terry") };
+	stack4.loadArrayOfObjects(yetAnotherPersonArray, 7);
+	cout << "\n*****************************\n";
+	cout << "Current stack4 status:" << endl;
+	cout << stack4;
+	cout << "\n*****************************\n";
+	cout << "\nReducing stack4 by reduntant last 8 elements:\n";
 
+	for (int i = 0; i < 8; i++)
+	{
+		cout << "\tPopping Person " << stack4.pop() << endl;
+	}
+
+	cout << "\n*****************************\n";
+	cout << "Current stack4 status:" << endl;
+	cout << stack4;
+	cout << "\n*****************************\n";
+	cout << "Now generating 2 Person objects and adding them to the stack4 using push(T) method, one by one:\n";
+	Person person1("Anthony"), person2("Brian");
+	stack4.push(person1);
+	stack4.push(person2);
+	cout << "\n*****************************\n";
+	cout << "Current stack4 status:" << endl;
+	cout << stack4;
+	cout << "\n\n\n";
 }
--- Stack Implementation Using Templates/stack.h
+++ Stack Implementation Using Templates/stack.h
@@ -11,6 +11,8 @@ class Stack;
 template <class T, int tabSize>
 ostream& operator<<(ostream& out, typename Stack<T, tabSize>& arg);
 
+enum CapacityModification {expand, reduce};
+
 
 template <class T, int tabSize>
 class Stack
@@ -18,13 +20,15 @@ class Stack
 	int stackCapacity;
 	T* arrayPtr;
 	int stackElementCounter;
+	string instanceName;
 public:
-	Stack()
+	Stack(string instanceName = "No-name object")
 	{
 		stackCapacity = tabSize;
 
 		arrayPtr = new T[stackCapacity];
 		stackElementCounter = 0;
+		this->instanceName = instanceName;
 	}
 
 	friend ostream& operator<< <T, tabSize> (ostream& out, typename Stack<T, tabSize>& arg);
@@ -33,16 +37,39 @@ public:
 	void push(T arg);
 	T pop();
 	int getElementCounter() { return stackElementCounter; }
+	string getInstanceName() { return instanceName; }
 	void loadArrayOfObjects(T arr[], int arraySize);
+	bool empty();
 private:
-	void expandStack();
+	void changeStackCapacity(CapacityModification action);    // used by pop() and push(T)
+	void changeStackCapacity(int expansionFactor);  // method overload for loadArrayOfObjects optimization
 };
 
+
+
+template <class T, int tabSize>
+bool Stack<T, tabSize>::empty()
+{
+	return stackElementCounter == 0 ? true : false;
+}
 
 template <class T, int tabSize>
 void Stack<T, tabSize>::loadArrayOfObjects(T arr[], int arraySize)
 {
-	for (int i = 0; i < arraySize; i++) {
+	if (stackElementCounter + arraySize > stackCapacity)
+	{
+		int expansionFactor = (stackElementCounter + arraySize) / tabSize;  // to check how much to expand the Stack before loading the input array
+
+		if ((stackElementCounter + arraySize) % tabSize != 0)    // if the eventual needed capacity is > n*tabSize and < (n+1)*tabSize
+		{
+			expansionFactor++;
+		}
+
+		this->changeStackCapacity(expansionFactor);
+	}
+
+	for (int i = 0; i < arraySize; i++) 
+	{
 		this->push(arr[i]);
 		cout << ".";
 	}
@@ -51,6 +78,12 @@ void Stack<T, tabSize>::loadArrayOfObjects(T arr[], int arraySize)
 template <class T, int tabSize>
 ostream& operator<<(ostream& out, typename Stack<T, tabSize>& arg)
 {
+	if (arg.empty())
+	{
+		out << "Stack " << arg.getInstanceName() << " is empty";
+		return out;
+	}
+
 	int elementNumber = arg.stackElementCounter;
 
 	for (int i = 0; i < elementNumber; i++)
@@ -66,14 +99,15 @@ ostream& operator<<(ostream& out, typename Stack<T, tabSize>& arg)
 template <class T, int tabSize>
 void Stack<T, tabSize>::push(T arg)
 {
-	if (stackElementCounter == stackCapacity)
+	stackElementCounter++;
+
+	if (stackElementCounter > stackCapacity)    // if the point of Stack overflow is reached
 	{
-		cout << "\nThe Stack has got full. This will now be expanded by additional " << tabSize << " fields\n";
-		expandStack();
+		cout << "\nThe Stack " << instanceName << " now needs to be expanded by additional " << tabSize << " fields\n";
+		changeStackCapacity(expand);
 	}
 
-	arrayPtr[stackElementCounter] = arg;
-	stackElementCounter++;
+	arrayPtr[stackElementCounter - 1] = arg;
 }
 
 
@@ -82,21 +116,30 @@ T Stack<T, tabSize>::pop()
 {
 	if (stackElementCounter <= 0)
 	{
-		cout << "\nThe Stack is empty\n";
+		cout << "\nThe Stack " << instanceName << " is empty\n";
 		return NULL;
 	}
 
+	int indexToPop = stackElementCounter - 1;
+	T objectToPop = arrayPtr[indexToPop];
+	stackElementCounter--;
 
-	return arrayPtr[--stackElementCounter];
+	if (stackElementCounter % tabSize == 0 && stackElementCounter >= tabSize)    // if the current element number decrements to the multiplicity of tabSize, excluding the single tabSize length
+	{
+		cout << "\nThe Stack " << instanceName << " can now be reduced by reduntant " << tabSize << " fields\n";
+		changeStackCapacity(reduce);
+	}
+
+	return objectToPop;
 }
 
 
 template <>
-string Stack<string, 10>::pop()     // template specialization to avoid the attempt to return NULL value as String
+string Stack<string, 10>::pop()     // full template specialization to avoid the attempt to return NULL value as String; due to partial template specialization the user can only use string Stacks of 10-element size, i.e. 10, 20, 30 etc. Not 5, 7, 11, etc. like for template arguments other than string 
 {
 	if (stackElementCounter <= 0)
 	{
-		cout << "\nThe Stack is empty\n";
+		cout << "\nThe Stack " << instanceName << " is empty\n";
 		return "";
 	}
 
@@ -105,16 +148,47 @@ string Stack<string, 10>::pop()     // template specialization to avoid the atte
 
 
 template <class T, int tabSize>
-void Stack<T, tabSize>::expandStack()
+void Stack<T, tabSize>::changeStackCapacity(CapacityModification action)
 {
-	stackCapacity += tabSize;
+	int howManyToRewrite = stackElementCounter;
+
+	if (action == expand)
+	{
+		stackCapacity += tabSize;
+		howManyToRewrite--;      // to avoid rewriting one object too many when expanding the Stack; found that bug during debugging
+	}
+	else
+	{
+		stackCapacity -= tabSize;
+	}
+	
 	T* newArray = new T[stackCapacity];
 
-	for (int i = 0; i < stackElementCounter; i++)
+	for (int i = 0; i < howManyToRewrite; i++)
 	{
 		newArray[i] = arrayPtr[i];
 	}
 
 	delete[] arrayPtr;
+	arrayPtr = newArray;
+}
+
+
+template <class T, int tabSize>
+void Stack<T, tabSize>::changeStackCapacity(int expansionFactor)
+{
+	stackCapacity = expansionFactor * tabSize;
+	T* newArray = new T[stackCapacity];
+
+	if (!this->empty())						// in case loadArrayOfObjects is used to add objects from an array to the Stack already containing some objects
+	{
+		for (int i = 0; i < stackElementCounter; i++)
+		{
+			newArray[i] = arrayPtr[i];
+		}
+
+		delete[] arrayPtr;
+	}
+
 	arrayPtr = newArray;
 }
